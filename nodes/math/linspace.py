57ddecb147b2f7331dfb4302fb783ac6dde97a7a
ly29 <linusyng@live.com>
2015-11-20 14:21:30 +0100

    Fix linspace

--- nodes/math/linspace.py
+++ nodes/math/linspace.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def linspace(start : float = 0.0, stop : float = 1.0, count : int = 10) -> [("linspace", np.array)]:
-    return np.linspace(start, stop, num)
+    return np.linspace(start, stop, count)
 
 linspace.label = "Linear space"
