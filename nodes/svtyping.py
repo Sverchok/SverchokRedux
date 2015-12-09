6d60951d05e17d79eebc9ab7c432a42298f93d46
ly29 <linusyng@live.com>
2015-12-09 18:49:39 +0100

    types
    first step

---
+++ nodes/svtyping.py
@@ -0,0 +1,29 @@
+class Topology:
+    pass
+
+class Vertex:
+    pass
+
+class Mesh:
+    pass
+
+class String:
+    pass
+
+class Color:
+    pass
+
+class Dict:
+    pass
+
+class Generic:
+    pass
+
+class Number:
+    pass
+
+class Float(Number):
+    pass
+
+class Integer(Number):
+    pass
