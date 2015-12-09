cc1477506c46d05ce745c3c5c2cbabdadd2ee403
ly29 <linusyng@live.com>
2015-12-09 10:14:22 +0100

    auto properties
    ugly code warning.

--- core/node.py
+++ core/node.py
@@ -6,6 +6,9 @@ import collections
 from ..ui.node import SvRxNode
 
 
+def execute_tree(self, context):
+    self.id_data.execute()
+
 NodeData = collections.namedtuple("NodeData", ['cls', 'func', 'category', 'inputs', 'outputs'])
 
 
@@ -28,19 +31,30 @@ def get_signature(func):
     annotations = func.__annotations__
     sig = inspect.signature(func)
     inputs_template = []
-    for name, parameter in sig.parameters.items():
+    properties = {}
+    items = list(sig.parameters.items())
+    for name, parameter in items:
+        if isinstance(annotations[name], tuple):  # and annotations[name][0].__name__.endswith("Propery"):
+            break
         s = (annotations[name], name, {"default_value": parameter.default})
         inputs_template.append(s)
-
+    for name, parameter in items:
+        print(name, parameter, annotations[name])
+        if not isinstance(annotations[name], tuple):
+            continue
+        prop = annotations[name]
+        prop[1].update({"name": name,
+                        "default": parameter.default,
+                        "update": execute_tree})
+        properties["svrx_{}".format(name)] = prop
     if not inputs_template:
         inputs_template = None
-
     ret_values = annotations.get("return")
     if ret_values:
         outputs_template = [(socket_type, name) for name, socket_type in ret_values]
     else:
         outputs_template = None
-    return inputs_template, outputs_template
+    return inputs_template, outputs_template, properties
 
 
 def node_factory_from_func(func):
@@ -50,10 +64,10 @@ def node_factory_from_func(func):
     class_name = "SvRxNode{}{}".format(module_name, func_name)
     bases = (SvRxNode, bpy.types.Node)
 
-    inputs, outputs = get_signature(func)
+    inputs, outputs, properties = get_signature(func)
     func.inputs = inputs
     func.outputs = outputs
-    node_dict = {}
+    node_dict = properties.copy()
 
     node_dict["bl_idname"] = class_name
     node_dict["bl_label"] = getattr(func, "label", func.__name__).title()
@@ -61,6 +75,7 @@ def node_factory_from_func(func):
 
     node_dict["inputs_template"] = [Socket(*data[:2], **data[-1]) for data in inputs] if inputs else None
     node_dict["outputs_template"] = [Socket(*data) for data in outputs] if outputs else None
+    node_dict["svrx_props"] = list(properties.keys())
 
     node_class = type(class_name, bases, node_dict)
     return NodeData(node_class, func, module_name, inputs, outputs)
