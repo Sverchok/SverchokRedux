3cd330f7522a81d9d5eb80dd397af26d81fdd3e1
ly29 <linusyng@live.com>
2015-12-15 11:27:13 +0100

    load nodes infrastructure from by.data.texts

---
+++ core/loadscript.py
@@ -0,0 +1,61 @@
+import importlib
+import importlib.abc
+import importlib.util
+import keyword
+import sys
+
+import bpy
+import bpy.types
+
+_script_modules = {}
+
+def load_script(text):
+    if text.endswith("py"):
+        name = text.rstrip(".py")
+    else:
+        name = text
+    if not name.isidentifier() or keyword.iskeyword(name):
+        print("bad text name: {}".format(text))
+        return
+
+    # we could introduce simple auto renamning or name mangling system
+    if name in _script_modules:
+        mod = _script_modules[name]
+        importlib.reload(mod)
+    else:
+        mod = importlib.import_module("SverchokRedux.nodes.script.{}".format(name))
+        _script_modules[name] = mod
+
+
+
+class SvRxFinder(importlib.abc.MetaPathFinder):
+
+    def find_spec(self, fullname, path, target=None):
+        if fullname.startswith("SverchokRedux.nodes.script."):
+            name = fullname.split(".")[-1]
+            name_py = "{}.py".format(name)
+            if name in bpy.data.texts:
+                return importlib.util.spec_from_loader(fullname, SvRxLoader(name))
+            elif name_py in bpy.data.texts:
+                return importlib.util.spec_from_loader(fullname, SvRxLoader(name_py))
+
+        elif fullname == "SverchokRedux.nodes.script":
+            # load Module, right now uses real but empty module, will perhaps change
+            pass
+
+        return None
+
+
+class SvRxLoader(importlib.abc.SourceLoader):
+
+    def __init__(self, text):
+        self._text = text
+
+    def get_data(self, path):
+        return bpy.data.texts[self._text].as_string()
+
+    def get_filename(self, fullname):
+        return "<bpy.data.texts[{}]>".format(self._text)
+
+
+sys.meta_path.append(SvRxFinder())
--- nodes/decorators.py
+++ nodes/decorators.py
@@ -1,15 +1,42 @@
 from ..nodes import _node_dict
-from SverchokRedux.core.factory import node_factory_from_func
+from ..core.factory import node_factory_from_func
+from ..ui import menu
 import bpy
+import bpy.types
 import traceback
 
-def register_node(func):
+
+def register_node(func, register_new=False):
     try:
         node_data = node_factory_from_func(func)
-        _node_dict[node_data.cls.bl_idname] = node_data
+        if register_new:
+            if hasattr(bpy.types, node_data.cls.bl_idname):
+                bpy.utils.unregister_class(node_data.cls)
+            bpy.utils.register_class(node_data.cls)
+            _node_dict[node_data.cls.bl_idname] = node_data
+            menu.reload_menu()
+        else:
+            _node_dict[node_data.cls.bl_idname] = node_data
+
     except Exception as err:
         print("Error: failed to load {}".format(func.__name__))
         traceback.print_tb(err.__traceback__)
+        raise err
+
+
+def node_script(*args, **values):
+    def real_node_func(func):
+        def annotate(func):
+            for key, value in values.items():
+                setattr(func, key, value)
+            return func
+        annotate(func)
+        register_node(func, register_new=True)
+        return func
+    if args and callable(args[0]):
+        return real_node_func(args[0])
+    else:
+        return real_node_func
 
 
 def node_func(*args, **values):
